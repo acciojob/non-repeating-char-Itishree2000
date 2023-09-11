d5d547465875b462fdd2ab59b540ec9bebfd1022
Itishree2000 <127124225+Itishree2000@users.noreply.github.com>
2023-09-11 19:37:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-}
+	let n=str.length;
+	if(n===0)
+	{
+		return null;
+	}
+	let i=str[0],j=str[0],firstNonRepeatedChar=null;
+	for(let i=0;i<n;i++){
+		let isRepeated=false;
+
+	for(let j=0;j<n;j++){
+		if(i!=j && str[i]===str[j])
+		{
+			isRepeated=true;
+			break;
+		}
+	}
+		if(!isRepeated)
+		{
+			firstNonRepeatedChar=str[i];
+			break;
+		}
+		return firstNonRepeatedChar;
+	}
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
